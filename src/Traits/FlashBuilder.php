91e54f9a5431203ff020dcb03453cbae1fc3dc18
Imani Manyara <imani@simtabi.com>
2021-11-20 22:13:30 -0500

    Bug fixes

--- src/Traits/FlashBuilder.php
+++ src/Traits/FlashBuilder.php
@@ -10,15 +10,6 @@ trait FlashBuilder
 
     public function fireFlashMessage(): FlashNotifier
     {
-
-        $notifier = app(LaratoastHelper::getFlashFacadeName());
-
-        if (! is_null($message)) {
-            return $notifier->message($message, $level);
-        }
-
-        return $notifier;
-
         return app(LaratoastHelper::getFlashFacadeName());
     }
 
